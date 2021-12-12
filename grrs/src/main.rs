2637df1d5da499bac663f87902125f9fcab0a4f4
testDrive64 <com9on@me.com>
2021-12-12 20:08:52 +0100

    Move lib.rs to src/.

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -13,17 +13,18 @@ fn main() -> Result<()> {
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
     find_matches(&content, &args.pattern, &mut std::io::stdout());
-
     Ok(())
 }
 
 
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write + std::fmt::Debug) {
     for line in content.lines() {
         if line.contains(pattern) {
-            writeln!(writer, "{}", line);
+            writeln!(&mut writer, "{}", line)
+                .with_context(|| format!("could not read file"));
         }
     }
+    println!("{:?}", writer);
 }
 
 
